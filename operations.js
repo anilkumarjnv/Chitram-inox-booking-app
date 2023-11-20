a5af37a73e2c80ea1e70faabea174231417c7164
anil <anilkumar2000jnv@gmail.com>
2023-11-20 16:16:26 +0530

    commit changes

--- operations.js
+++ operations.js
@@ -4,23 +4,23 @@ import mysql from 'mysql2';
 
 
 const connection = mysql.createPool({
-    host:'sql12.freesqldatabase.com',
-    user : 'sql12652212',
-    password: 'kp5mfNWI8X',
-    database: 'sql12652212'
+    host:'127.0.0.1',
+    user : 'root',
+    password: 'Anil1531@',
+    database:'my_db'
 }).promise()
 
 export async function readUser(username) {
-    const output = await connection.query("select * from user_details where username=?",[username])
+    const output = await connection.query("select * from users where username=?",[username])
     return output[0];
     
 }
 
 export async function insertUser(username,name,password){
-    const result = await connection.query('INSERT INTO user_details (username,name,password) VALUES (?,?,?)', [username,name,password]);
+    const result = await connection.query('INSERT INTO users (username,name,password) VALUES (?,?,?)', [username,name,password]);
 }
 export async function checkUsername(username){
-    const result = await connection.query('SELECT * FROM user_details WHERE username = ?', [username]);
+    const result = await connection.query('SELECT * FROM users WHERE username = ?', [username]);
     return(result[0].length)
 }
 
